1a5cfd59b9e87735f68d2942dc3ac085dc9f7908
Mihai Goncear <m.goncearm@gmail.com>
2024-12-14 21:01:35 +0200

    contacts page init

---
+++ src/Components/Pages/Contacts/Contacts.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import "./Contacts.sass";
+import { CONTACTS } from "utils/constants";
+import Contact from "Components/Footer/Components/Contact";
+
+function Contacts() {
+    return (
+        <div className='contacts'>
+            <div className='general-wrapper'>
+                <div className='ui__title'>Contacte</div>
+                <div className='contacts__wrapper'>
+                    <div className='contacts__contacts'>
+                        {CONTACTS.map((contact, index) => (
+                            <Contact
+                                info={contact}
+                                key={index}
+                            />
+                        ))}
+                    </div>
+                    <div className='contacts__reclamation'>
+                        <div className='title'>Lăsați-ne un mesaj:</div>
+                        <div className='input-wrapper'>
+                            <label htmlFor='name'>Numele și prenumele:</label>
+                            <input
+                                type='text'
+                                name='name'
+                                placeholder='Scrieți numele și prenumele...'
+                            />
+                        </div>
+                        <div className='input-wrapper'>
+                            <label htmlFor='email'>Email:</label>
+                            <input
+                                type='email'
+                                name='email'
+                                placeholder='Scrieți email-ul...'
+                            />
+                        </div>
+                        <div className='input-wrapper'>
+                            <label htmlFor='phone'>Telefon:</label>
+                            <input
+                                type='tel'
+                                name='phone'
+                                placeholder='Scrieți telefonul...'
+                            />
+                        </div>
+                        <div className='input-wrapper'>
+                            <label htmlFor='message'>Mesaj:</label>
+                            <textarea
+                                name='message'
+                                placeholder='Scrieți mesajul...'
+                            ></textarea>
+                        </div>
+                        <div className='input-wrapper'>
+                            <button>Trimite</button>
+                        </div>
+                    </div>
+                </div>
+                <div className='contacts__map'></div>
+            </div>
+        </div>
+    );
+}
+
+export default Contacts;
